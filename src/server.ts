2adf41dd01b08dda71ea7b148350a88266e692b0
Raphael Pimenta Gomes <raphael.pimentagomes@hotmail.com>
2024-11-27 17:05:19 -0300

    request e reply da resquisição

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 
@@ -9,8 +15,27 @@ const app = fastify()
 // Parâmetros de rota (route Params) -> Identificação de recursos 
 // Cabeçalhos (Headers) -> Contexto
 
-app.post('/events', () => {
-    return 'Hello Raphael'
+app.post('/events', async (resquest, reply) => {
+    
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(resquest.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        }
+    })
+
+
+    return reply.status(201).send({ eventId: event.id})
 })
 
 
