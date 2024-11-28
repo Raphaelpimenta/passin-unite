aa722aa63cea4560a2d1e1f68c1058aff4ed2988
Raphael Pimenta Gomes <raphael.pimentagomes@hotmail.com>
2024-11-28 16:15:39 -0300

    Criação de rota de cadastro de eventos e participantes

---
+++ README.md
@@ -0,0 +1,17 @@
+## Anotações
+
+Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
+
+Corpo da requisição (Request body)
+Parâmetros de busca (Search Params / Query Params) `http//localhost:3333/users?name=Raphael`
+Parâmetros de rota (route Params) -> Identificação de recursos 
+Cabeçalhos (Headers) -> Contexto
+
+
+## Dependências
+
+- npm init -y --> Vai criar uma json do projeto inicial
+
+- npm i tsx --> Typescript
+- npm i prisma
+- npm i fastify-type-provider-zod
--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,16 @@
 import fastify from "fastify";
-import z from "zod";
-import { PrismaClient } from "@prisma/client";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
-const app = fastify()
+export const app = fastify()
 
-const prisma = new PrismaClient({
-    log: ['query'],
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
+app.register(createEvent)
+app.register(registerForEvent)
 
-// Corpo da requisição (Request body)
-// Parâmetros de busca (Search Params / Query Params) `http//localhost:3333/users?name=Raphael`
-// Parâmetros de rota (route Params) -> Identificação de recursos 
-// Cabeçalhos (Headers) -> Contexto
-
-app.post('/events', async (resquest, reply) => {
-    
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-    })
-
-    const data = createEventSchema.parse(resquest.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendees: data.maximumAttendees,
-            slug: new Date().toISOString(),
-        }
-    })
-
-
-    return reply.status(201).send({ eventId: event.id})
-})
 
 
 app.listen({ port: 3333 }).then(() => {
